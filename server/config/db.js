a1cbfdaec8f094a7f52b712095cdb9589c467fc4
AsiaHotels-IT <itco@asiahotel.co.th>
2025-10-01 15:10:18 +0700

    011025

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://dev:AsiadevBkk@10.10.0.39:27017/tennis');
+        await mongoose.connect('mongodb://dev:AsiadevBkk@10.10.0.39:27017/tennis?authSource=admin');
         console.log('DB connected');
     } catch (err) {
         console.log('MongoDB connection error:', err.message);
